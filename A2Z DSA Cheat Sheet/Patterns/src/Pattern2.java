be64685958c8292e818d37e23dfaaeb59881bdad
mathwog2003 <gowthamselvaraj037@gmail.com>
2023-12-28 20:32:51 +0530

    Second patter solved

---
+++ A2Z DSA Cheat Sheet/Patterns/src/Pattern2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Pattern2 {
+
+    public static void pattern(int a)
+    {
+        for(int i = 0 ; i < a ; i++)
+        {
+            for(int j = 0 ; j <= i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        pattern(input);
+    }
+}
