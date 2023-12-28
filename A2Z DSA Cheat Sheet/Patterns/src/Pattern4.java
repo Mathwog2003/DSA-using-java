3d8bf13b6d7e049403e727de96fe7001757965c5
mathwog2003 <gowthamselvaraj037@gmail.com>
2023-12-28 21:28:03 +0530

    solved pattern 4 and 5

--- A2Z DSA Cheat Sheet/Patterns/src/Pattern4.java
+++ A2Z DSA Cheat Sheet/Patterns/src/Pattern4.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class Pattern3 {
+public class Pattern4 {
 
     public static void square(int a) {
         for(int i = 1 ; i < a ; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(i);
             }
             System.out.println();
         }
