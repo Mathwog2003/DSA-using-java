f2327a281e8310582866acc808093d8fbc6f3cc7
mathwog2003 <gowthamselvaraj037@gmail.com>
2023-12-30 07:12:14 +0530

    solved pattern7

--- A2Z DSA Cheat Sheet/Patterns/src/Pattern7.java
+++ A2Z DSA Cheat Sheet/Patterns/src/Pattern7.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 public class Pattern7 {
 
     public static void square(int a) {
-        for(int i = 1 ; i <= a ; i++)
+        for(int i = 0 ; i < a ; i++)
         {
-            for(int j = a ; j >= i ; j--)
+            for(int j = 0 ; j < a-i-1 ; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 0 ; j < 2*i+1 ; j++)
             {
                 System.out.print("*");
+            }
+            for(int j = 0 ; j < a-i-1 ; j++)
+            {
+                System.out.print(" ");
             }
             System.out.println();
         }
