e0c564a02f7260937c92cae9825833f6dfdb49bc
mathwog2003 <gowthamselvaraj037@gmail.com>
2023-12-30 06:31:10 +0530

    solved pattern t

---
+++ A2Z DSA Cheat Sheet/Patterns/src/Pattern7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pattern7 {
+
+    public static void square(int a) {
+        for(int i = 1 ; i <= a ; i++)
+        {
+            for(int j = a ; j >= i ; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        square(input);
+    }
+}
