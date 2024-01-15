473979d8822a778fcde18de177ee9ac4c7e4c0d3
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-15 14:02:09 +0530

    reformat

---
+++ A2Z DSA Cheat Sheet/Patterns/src/Pattern12.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pattern12 {
+    public static void square(int N)
+    {
+
+    }
+
+
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        square(userInput);
+
+    }
+}
