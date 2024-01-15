8888cc0682394f6184113e30d0352557e0550a4d
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-15 14:19:26 +0530

    added comments

--- A2Z DSA Cheat Sheet/BasicMath/src/CountDigits.java
+++ A2Z DSA Cheat Sheet/BasicMath/src/CountDigits.java
@@ -13,6 +13,9 @@ public class CountDigits {
         //     x = x/10;
         // }
         // return count;
+
+        //Time complexity is o(log10(X))
+        //Number of iteration is based on division TC will be logarithmetic
         int count = (int)(Math.log10(x)+1);
         return count;
     }
