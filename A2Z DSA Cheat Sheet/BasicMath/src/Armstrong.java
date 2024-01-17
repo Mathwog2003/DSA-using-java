cba175ac1cfaaede7acd74fdd417c11215c5941e
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-17 10:17:44 +0530

    solved armstrong number problem

---
+++ A2Z DSA Cheat Sheet/BasicMath/src/Armstrong.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Armstrong {
+
+    public static void main(String[] args) {
+        // Write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        System.out.println(armstrong(input));
+    }
+    public static boolean armstrong(int N)
+    {
+        int arm = 0;
+        int ls = 0;
+        int count = 0;
+        int dup = N;
+        while(N>0)
+        {
+            N = N/10;
+            count++;
+        }
+        N = dup;
+        while(N>0)
+        {
+            ls = N%10;
+            arm = (int)(arm + (Math.pow(ls, count)));
+            N = N/10;
+        }
+        if(arm == dup)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
