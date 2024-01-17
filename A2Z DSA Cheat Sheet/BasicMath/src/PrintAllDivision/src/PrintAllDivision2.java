9553925aaa599981c1d6cfaf34bd963795d1c828
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-17 17:07:49 +0530

    solved the print all division problem with opticial approach with the TC 0(sqrt(n))

---
+++ A2Z DSA Cheat Sheet/BasicMath/src/PrintAllDivision/src/PrintAllDivision2.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+public class PrintAllDivision2{
+    public static List< Integer > printDivisors(int n) {
+
+        List<Integer> divisors = new ArrayList<>();
+
+        for(int i = 1; i<=(int)(Math.sqrt(n)) ; i++)
+        {
+            if(n%i == 0)
+            {
+                divisors.add(i);
+                if(i != n/i)
+                {
+                    divisors.add(n/i);
+                }
+            }
+            Collections.sort(divisors);
+        }
+        return divisors;
+
+    }
+}
