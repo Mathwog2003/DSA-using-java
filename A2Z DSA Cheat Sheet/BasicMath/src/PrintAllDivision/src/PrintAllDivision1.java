186c022e432cf237ff68be83d9b131c48892cdb7
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-17 17:00:31 +0530

    solved printAllDivision probelm with bruite force approach with TC o(n)

---
+++ A2Z DSA Cheat Sheet/BasicMath/src/PrintAllDivision/src/PrintAllDivision1.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+public class PrintAllDivision1{
+    public static List< Integer > printDivisors(int n) {
+
+        List<Integer> divisors = new ArrayList<>();
+
+        for(int i = 1; i <= n ; i++)
+        {
+            if(n%i == 0)
+            {
+                divisors.add(i);
+            }
+        }
+        return divisors;
+
+    }
+}
