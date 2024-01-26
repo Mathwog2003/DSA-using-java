151243df33a263464b04b690221d35b68917e55c
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-26 13:09:30 +0530

    solved gcd problem

---
+++ A2Z DSA Cheat Sheet/BasicMath/src/GCD_HCD.java
@@ -0,0 +1,24 @@
+
+public class GCD_HCD {
+	
+	public static int gcd(int a,int b)
+	{
+		// Write your code here
+		while(a>0 && b>0)
+		{
+			if(a>b)
+			{
+				a = a%b;
+			}
+			else
+			{
+				b = b%a;
+			}
+		}
+		if(a==0)
+		{
+			return b;
+		}
+		return a;
+	}		
+}
