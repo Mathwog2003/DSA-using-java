15f943ec3ebd8042663c246012ded89835fc7709
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-01-31 23:09:51 +0530

    solved factorial using recurion

---
+++ A2Z DSA Cheat Sheet/BaiscRecursion/src/FactorialUsingRecursion.java
@@ -0,0 +1,14 @@
+public class FactorialUsingRecursion {
+    public static long sumFirstN(long n) {
+        // Write your code here.
+        if(n == 0)
+        {
+            return 1;
+        }
+        return n * sumFirstN(n-1);
+    }
+    public static void main(String args[])
+    {
+        System.out.println(sumFirstN(3));
+    }
+}
